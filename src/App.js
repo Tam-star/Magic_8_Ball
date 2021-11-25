572cb9ff97fd9563e21e4cec8adf270e38703489
Tamara Wenner <tamara-76@hotmail.fr>
2021-11-25 14:34:32 +0100

    Implémentation du premier onclick event

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import logo from './logo.svg';
+import React from 'react';
+
+let myElement = [Question, MagicBall, Form]
+let numberElement=0;
+
+function nextElement(){
+  console.log(numberElement)
+  numberElement+=1;
+}
+
+function appearMagiBall(){
+
+}
 
 function App() {
+ 
   return (
     <>
       <Header></Header>
-      <Question></Question>
-      <MagicBall></MagicBall>
-      <Form></Form>
+      <Main></Main>
       <Footer></Footer>
     </>
   );
 }
+
+
 
 
 function Header(){
@@ -18,27 +32,53 @@ function Header(){
   <header>
     <div className="header-first-part">
       <img src={logo} alt='logo' style={{width:'100px'}}/>
-      <h1>Welcome to my first React website</h1>
+      <h1>Magic-8-Ball</h1>
+      <img src={logo} alt='logo' style={{width:'100px'}}/>
     </div>
-    <h2>Ask your question</h2>
+    <h2>Ask your question...</h2>
   </header>
   );
 }
 
-function Question(){
+
+
+function Main(){
+  // const [mainElement, setMainElement] = React.useState(myElement[numberElement]);
+  // function changeMainElement(){
+  //   setMainElement(myElement[numberElement])
+  // }
+  const[visibleQuestion, setVisibleQuestion] = React.useState({display:'block'});
+  const[visibleMagicBall, setVisibleMagicBall] = React.useState({display:'none', backgroundColor:'red'});
+
+  console.log(visibleMagicBall);
+
+  function handleClick(){
+    setVisibleQuestion({display:'none'})
+    setVisibleMagicBall({display:'block'})
+  }
+
   return(
-    <div className="question">
+    <main>
+      <Question buttonFct={handleClick} style={visibleQuestion}></Question>
+      <MagicBall style={visibleMagicBall}></MagicBall>
+    </main>
+  )
+}
+
+function Question({buttonFct, style}){
+  return(
+    <div className="question" style={style}>
         <h2>Vous avez une question, non ?</h2>
-        <button>Oui</button>
+        <button onClick={buttonFct}>Oui</button>
         <button>Non</button>
     </div>
     )
 }
 
-function MagicBall(){
+function MagicBall({style}){
   return(
-    <div>
-      <div className="magic-ball">
+    <div style={style}>
+      <div className="magic-ball" >
           <div className="answer">Impossible</div>
           <div className="number-eight">8</div>
       </div>
@@ -49,15 +89,15 @@ function MagicBall(){
 function Form(){
   return(
     <form>
-      <label for="last-name">Votre nom ? </label>
+      <label htmlFor="last-name">Votre nom ? </label>
       <input id="last-name"></input>
-      <label for="first-name">Votre prénom ? </label>
+      <label htmlFor="first-name">Votre prénom ? </label>
       <input id="first-name"></input>
-      <label for="age">Votre âge ? </label>
+      <label htmlFor="age">Votre âge ? </label>
       <input id="age"></input>
-      <label for="country">Votre pays ? </label>
+      <label htmlFor="country">Votre pays ? </label>
       <input id="country"></input>
-      <label for="email">Votre email ? </label>
+      <label htmlFor="email">Votre email ? </label>
       <input id="email"></input>
     </form>
     );
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+    <App mainElement="<p>Coco</p>"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
